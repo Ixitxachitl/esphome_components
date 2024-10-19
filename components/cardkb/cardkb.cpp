2bc1f3acfeba554bea5c3fb5936119b5f23f6cd6
Noah <kramerfm@gmail.com>
2024-10-19 11:12:08 -0700

    Update cardkb.cpp

--- components/cardkb/cardkb.cpp
+++ components/cardkb/cardkb.cpp
@@ -9,24 +9,27 @@ static const char *TAG = "cardkb";
 
 void CardKB::loop() {
   uint8_t c;
-  static uint32_t last_error = 0;
   c = this->pressed_key_;
   if (c) {
     for (auto &listener : this->listeners_)
       listener->key_released(c);
     this->pressed_key_ = 0;
   }
+
   i2c::ErrorCode res = this->read(&c, 1);
   if ((res != i2c::NO_ERROR) || (c == 0))
     return;
+
   if (c < 32)
     ESP_LOGD(TAG, "keycode '%d' pressed", c);
   else
     ESP_LOGD(TAG, "key '%c' pressed", c);
-  for (auto &listener : this->listeners_)
+
+  for (auto &listener : this->listeners_) {
     listener->key_pressed(c);
+  }
+
   this->pressed_key_ = c;
-  this->send_key_(c);
 }
 
 void CardKB::dump_config() {
@@ -39,4 +42,3 @@ void CardKB::register_listener(CardKBListener *listener) {
 
 }  // namespace cardkb
 }  // namespace esphome
-
