8dc592878f0d4d75395737b17e44ce79ae2c0eca
Noah <kramerfm@gmail.com>
2024-10-19 11:18:34 -0700

    Update __init__.py

--- components/cardkb/text_sensor/__init__.py
+++ components/cardkb/text_sensor/__init__.py
@@ -5,11 +5,11 @@ from esphome.const import CONF_ID
 
 cardkb_ns = cg.esphome_ns.namespace('cardkb')
 CardKB = cardkb_ns.class_('CardKB', cg.Component, i2c.I2CDevice)
-CardKBTextSensor = cardkb_ns.class_('CardKBTextSensor', text_sensor.TextSensor, cg.Component)
+CardKBTextSensor = cardkb_ns.class_('CardKBTextSensor', text_sensor.TextSensor)
 
 CONF_CARDKB_ID = 'cardkb_id'
 
-CONFIG_SCHEMA = i2c.i2c_device_schema(None).extend(
+CONFIG_SCHEMA = i2c.i2c_device_schema().extend(
     {
         cv.GenerateID(CONF_CARDKB_ID): cv.declare_id(CardKB),
         cv.Required(CONF_ID): cv.declare_id(CardKBTextSensor),
@@ -17,11 +17,13 @@ CONFIG_SCHEMA = i2c.i2c_device_schema(None).extend(
 )
 
 async def to_code(config):
+    # Initialize CardKB
     cardkb_var = cg.new_Pvariable(config[CONF_CARDKB_ID])
     await cg.register_component(cardkb_var, config)
     await i2c.register_i2c_device(cardkb_var, config)
 
+    # Initialize CardKBTextSensor
     text_sensor_var = cg.new_Pvariable(config[CONF_ID])
-    await text_sensor.register_text_sensor(text_sensor_var, config)
     cg.add(cardkb_var.register_listener(text_sensor_var))
-    await cg.register_component(text_sensor_var, config)  # Ensure proper registration
+    await text_sensor.register_text_sensor(text_sensor_var, config)
+    await cg.register_component(text_sensor_var, config)  # Register as a component
