de8388a8fa481db1b8049c845a0b50178f8051df
Noah <kramerfm@gmail.com>
2024-10-19 11:20:09 -0700

    Update __init__.py

--- components/cardkb/text_sensor/__init__.py
+++ components/cardkb/text_sensor/__init__.py
@@ -5,7 +5,7 @@ from esphome.const import CONF_ID
 
 cardkb_ns = cg.esphome_ns.namespace('cardkb')
 CardKB = cardkb_ns.class_('CardKB', cg.Component, i2c.I2CDevice)
-CardKBTextSensor = cardkb_ns.class_('CardKBTextSensor', text_sensor.TextSensor)
+CardKBTextSensor = cardkb_ns.class_('CardKBTextSensor', text_sensor.TextSensor, cg.Component)
 
 CONF_CARDKB_ID = 'cardkb_id'
 
@@ -17,13 +17,14 @@ CONFIG_SCHEMA = i2c.i2c_device_schema().extend(
 )
 
 async def to_code(config):
-    # Initialize CardKB
     cardkb_var = cg.new_Pvariable(config[CONF_CARDKB_ID])
     await cg.register_component(cardkb_var, config)
     await i2c.register_i2c_device(cardkb_var, config)
 
-    # Initialize CardKBTextSensor
     text_sensor_var = cg.new_Pvariable(config[CONF_ID])
     cg.add(cardkb_var.register_listener(text_sensor_var))
     await text_sensor.register_text_sensor(text_sensor_var, config)
-    await cg.register_component(text_sensor_var, config)  # Register as a component
+    await cg.register_component(text_sensor_var, config)
+
+    # Add debug logging
+    cg.add_global(cg.RawExpression('ESP_LOGD("cardkb", "CardKBTextSensor initialized")'))
