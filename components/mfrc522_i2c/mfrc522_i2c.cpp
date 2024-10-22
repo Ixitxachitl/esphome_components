573295084f3a901cd3fbc08f4e85fa5e0e84cda9
Noah <kramerfm@gmail.com>
2024-10-22 08:44:20 -0700

    test

--- components/mfrc522_i2c/mfrc522_i2c.cpp
+++ components/mfrc522_i2c/mfrc522_i2c.cpp
@@ -13,34 +13,34 @@ void MFRC522I2C::dump_config() {
 }
 
 bool MFRC522I2C::read_full_uid(uint8_t *uid, uint8_t *uid_length) {
-  // Send Anti-collision command
-  this->pcd_write_register(0x0D, 0x93);  // CommandReg = PCD_Anticoll (0x93)
+  // Send Anti-collision command using CommandReg
+  this->pcd_write_register(CommandReg, 0x93);  // PCD_Anticoll (0x93)
   
-  // Read UID from FIFODataReg (register 0x09)
+  // Read UID from FIFODataReg
   *uid_length = 5;  // Assuming a 5-byte UID
-  this->pcd_read_register(0x09, *uid_length, uid, 0);  // FIFODataReg is 0x09
+  this->pcd_read_register(FIFODataReg, *uid_length, uid, 0);
 
   ESP_LOGI(TAG, "UID: %s", format_hex_pretty(uid, *uid_length).c_str());
   return true;
 }
 
 bool MFRC522I2C::read_sak(uint8_t *sak) {
-  // Send SELECT command to get SAK
-  this->pcd_write_register(0x0D, 0x70);  // CommandReg = PCD_Select (0x70)
+  // Send SELECT command using CommandReg
+  this->pcd_write_register(CommandReg, 0x70);  // PCD_Select (0x70)
 
   // Read SAK from FIFODataReg
-  *sak = this->pcd_read_register(0x09);  // FIFODataReg is 0x09
+  *sak = this->pcd_read_register(FIFODataReg);
   ESP_LOGI(TAG, "SAK: 0x%02X", *sak);
   return true;
 }
 
 bool MFRC522I2C::read_atqa(uint16_t *atqa) {
-  // Send REQA command to get ATQA
-  this->pcd_write_register(0x0D, 0x26);  // CommandReg = PCD_REQA (0x26)
+  // Send REQA command using CommandReg
+  this->pcd_write_register(CommandReg, 0x26);  // PCD_REQA (0x26)
 
   // Read ATQA from FIFODataReg
   uint8_t atqa_buf[2] = {0};
-  this->pcd_read_register(0x09, 2, atqa_buf, 0);  // FIFODataReg is 0x09
+  this->pcd_read_register(FIFODataReg, 2, atqa_buf, 0);
   *atqa = (atqa_buf[0] << 8) | atqa_buf[1];
 
   ESP_LOGI(TAG, "ATQA: 0x%04X", *atqa);
--- components/mfrc522_i2c/mfrc522_i2c.h
+++ components/mfrc522_i2c/mfrc522_i2c.h
@@ -7,11 +7,17 @@
 namespace esphome {
 namespace mfrc522_i2c {
 
+enum PcdRegister {
+  CommandReg = 0x0D,
+  FIFODataReg = 0x09,
+  ControlReg = 0x0C,
+  // Add more registers as needed
+};
+
 class MFRC522I2C : public rc522::RC522, public i2c::I2CDevice {
  public:
   void dump_config() override;
 
-  // Add new methods for reading more tag data
   bool read_full_uid(uint8_t *uid, uint8_t *uid_length);
   bool read_sak(uint8_t *sak);
   bool read_atqa(uint16_t *atqa);
