d41a237d6816e99ababb2b401c979c481c9fbbae
Noah <kramerfm@gmail.com>
2024-10-22 01:28:29 -0700

    test

--- components/mfrc522_i2c/mfrc522_i2c.h
+++ components/mfrc522_i2c/mfrc522_i2c.h
@@ -1,8 +1,7 @@
 #pragma once
 
-#include "esphome/core/component.h"
-#include "esphome/components/rc522/rc522.h"
 #include "esphome/components/i2c/i2c.h"
+#include "esphome/components/rc522/rc522.h"
 
 namespace esphome {
 namespace mfrc522_i2c {
@@ -11,19 +10,17 @@ class MFRC522I2C : public rc522::RC522, public i2c::I2CDevice {
  public:
   void dump_config() override;
 
-  // Getter methods to access UID and FIFO data
+  uint8_t pcd_read_register(PcdRegister reg) override;
+  void pcd_read_register(PcdRegister reg, uint8_t count, uint8_t *values, uint8_t rx_align) override;
+  void pcd_write_register(PcdRegister reg, uint8_t value) override;
+  void pcd_write_register(PcdRegister reg, uint8_t count, uint8_t *values) override;
+
   std::string get_uid();
   std::string get_fifo_data_string();
+  bool request_tag(PICC_Command command);
+  bool anti_collision(uint8_t *buffer, uint8_t *buffer_size);
 
  protected:
-  uint8_t pcd_read_register(rc522::RC522::PcdRegister reg) override;
-  void pcd_read_register(rc522::RC522::PcdRegister reg, uint8_t count, uint8_t *values, uint8_t rx_align) override;
-  void pcd_write_register(rc522::RC522::PcdRegister reg, uint8_t value) override;
-  void pcd_write_register(rc522::RC522::PcdRegister reg, uint8_t count, uint8_t *values) override;
-
- private:
-  bool request_tag(rc522::RC522::PICC_Command command);
-  bool anti_collision(uint8_t *buffer, uint8_t *buffer_size);
   std::string uid_;
   std::string fifo_data_;
 };
