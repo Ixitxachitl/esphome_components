6adc28a0de9af92a7cf4bbf985eb359e1ecb98fc
Noah <kramerfm@gmail.com>
2024-10-22 01:39:39 -0700

    test

--- components/mfrc522_i2c/mfrc522_i2c.cpp
+++ components/mfrc522_i2c/mfrc522_i2c.cpp
@@ -3,33 +3,35 @@
 namespace esphome {
 namespace mfrc522_i2c {
 
+static const char *TAG = "mfrc522_i2c";
+
 void MFRC522I2C::dump_config() {
   ESP_LOGCONFIG(TAG, "MFRC522I2C:");
   ESP_LOGCONFIG(TAG, "  Address: 0x%02X", this->address_);
 }
 
-uint8_t MFRC522I2C::pcd_read_register(PcdRegister reg) {
+uint8_t MFRC522I2C::pcd_read_register(rc522::RC522::PcdRegister reg) {
   uint8_t value;
   this->read_bytes(reg, &value, 1);
   return value;
 }
 
-void MFRC522I2C::pcd_read_register(PcdRegister reg, uint8_t count, uint8_t *values, uint8_t rx_align) {
+void MFRC522I2C::pcd_read_register(rc522::RC522::PcdRegister reg, uint8_t count, uint8_t *values, uint8_t rx_align) {
   this->read_bytes(reg, values, count);
 }
 
-void MFRC522I2C::pcd_write_register(PcdRegister reg, uint8_t value) {
+void MFRC522I2C::pcd_write_register(rc522::RC522::PcdRegister reg, uint8_t value) {
   this->write_bytes(reg, &value, 1);
 }
 
-void MFRC522I2C::pcd_write_register(PcdRegister reg, uint8_t count, uint8_t *values) {
+void MFRC522I2C::pcd_write_register(rc522::RC522::PcdRegister reg, uint8_t count, uint8_t *values) {
   this->write_bytes(reg, values, count);
 }
 
 std::string MFRC522I2C::get_uid() {
   uint8_t buffer[10];
   uint8_t buffer_size = sizeof(buffer);
-  if (this->request_tag(PiccCommand::PICC_CMD_REQA) && this->anti_collision(buffer, &buffer_size)) {
+  if (this->request_tag(rc522::RC522::PiccCommand::PICC_CMD_REQA) && this->anti_collision(buffer, &buffer_size)) {
     this->uid_ = std::string(buffer, buffer + buffer_size);
   } else {
     this->uid_.clear();
@@ -38,23 +40,23 @@ std::string MFRC522I2C::get_uid() {
 }
 
 std::string MFRC522I2C::get_fifo_data_string() {
-  uint8_t fifo_size = this->pcd_read_register(PcdRegister::FIFO_LEVEL_REG);
+  uint8_t fifo_size = this->pcd_read_register(rc522::RC522::PcdRegister::FIFO_LEVEL_REG);
   uint8_t buffer[64];
-  this->pcd_read_register(PcdRegister::FIFO_DATA_REG, fifo_size, buffer, 0);
+  this->pcd_read_register(rc522::RC522::PcdRegister::FIFO_DATA_REG, fifo_size, buffer, 0);
   this->fifo_data_ = std::string(buffer, buffer + fifo_size);
   return this->fifo_data_;
 }
 
-bool MFRC522I2C::request_tag(PcdCommand command) {
-  this->pcd_write_register(PcdRegister::BIT_FRAMING_REG, 0x07);
+bool MFRC522I2C::request_tag(rc522::RC522::PiccCommand command) {
+  this->pcd_write_register(rc522::RC522::PcdRegister::BIT_FRAMING_REG, 0x07);
   uint8_t command_buffer[] = {static_cast<uint8_t>(command)};
-  this->pcd_transceive_data_(command_buffer, sizeof(command_buffer));
+  this->pcd_transceive_data_(command_buffer[0]);
   return true;
 }
 
 bool MFRC522I2C::anti_collision(uint8_t *buffer, uint8_t *buffer_size) {
-  this->pcd_write_register(PcdRegister::BIT_FRAMING_REG, 0x00);
-  this->pcd_read_register(PcdRegister::FIFO_DATA_REG, *buffer_size, buffer, 0);
+  this->pcd_write_register(rc522::RC522::PcdRegister::BIT_FRAMING_REG, 0x00);
+  this->pcd_read_register(rc522::RC522::PcdRegister::FIFO_DATA_REG, *buffer_size, buffer, 0);
   return true;
 }
 
--- components/mfrc522_i2c/mfrc522_i2c.h
+++ components/mfrc522_i2c/mfrc522_i2c.h
@@ -1,28 +1,23 @@
 #pragma once
 
-#include "esphome/core/component.h"
-#include "esphome/core/hal.h"
-#include "esphome/components/i2c/i2c.h"
 #include "esphome/components/rc522/rc522.h"
+#include "esphome/core/component.h"
 
 namespace esphome {
 namespace mfrc522_i2c {
-
-using namespace rc522;
 
 class MFRC522I2C : public rc522::RC522, public i2c::I2CDevice {
  public:
   void dump_config() override;
 
-  uint8_t pcd_read_register(PcdRegister reg) override;
-  void pcd_read_register(PcdRegister reg, uint8_t count, uint8_t *values, uint8_t rx_align) override;
-  void pcd_write_register(PcdRegister reg, uint8_t value) override;
-  void pcd_write_register(PcdRegister reg, uint8_t count, uint8_t *values) override;
+  uint8_t pcd_read_register(rc522::RC522::PcdRegister reg) override;
+  void pcd_read_register(rc522::RC522::PcdRegister reg, uint8_t count, uint8_t *values, uint8_t rx_align) override;
+  void pcd_write_register(rc522::RC522::PcdRegister reg, uint8_t value) override;
+  void pcd_write_register(rc522::RC522::PcdRegister reg, uint8_t count, uint8_t *values) override;
 
   std::string get_uid();
   std::string get_fifo_data_string();
-
-  bool request_tag(PcdCommand command);
+  bool request_tag(rc522::RC522::PiccCommand command);
   bool anti_collision(uint8_t *buffer, uint8_t *buffer_size);
 
  protected:
