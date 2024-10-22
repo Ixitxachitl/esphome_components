a1bddbe0a882ac9ae3f88ff1b8ad2bc1702d25eb
Noah <kramerfm@gmail.com>
2024-10-22 01:42:03 -0700

    test

--- components/mfrc522_i2c/mfrc522_i2c.h
+++ components/mfrc522_i2c/mfrc522_i2c.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include "esphome/components/rc522/rc522.h"
 #include "esphome/core/component.h"
+#include "esphome/components/rc522/rc522.h"
+#include "esphome/components/i2c/i2c.h"
 
 namespace esphome {
 namespace mfrc522_i2c {
@@ -10,10 +11,10 @@ class MFRC522I2C : public rc522::RC522, public i2c::I2CDevice {
  public:
   void dump_config() override;
 
-  uint8_t pcd_read_register(rc522::RC522::PcdRegister reg) override;
-  void pcd_read_register(rc522::RC522::PcdRegister reg, uint8_t count, uint8_t *values, uint8_t rx_align) override;
-  void pcd_write_register(rc522::RC522::PcdRegister reg, uint8_t value) override;
-  void pcd_write_register(rc522::RC522::PcdRegister reg, uint8_t count, uint8_t *values) override;
+  uint8_t pcd_read_register(rc522::RC522::PcdRegister reg);
+  void pcd_read_register(rc522::RC522::PcdRegister reg, uint8_t count, uint8_t *values, uint8_t rx_align);
+  void pcd_write_register(rc522::RC522::PcdRegister reg, uint8_t value);
+  void pcd_write_register(rc522::RC522::PcdRegister reg, uint8_t count, uint8_t *values);
 
   std::string get_uid();
   std::string get_fifo_data_string();
