1fdd44f30619fbd978098d3ff31cda874bb2aae8
Noah <kramerfm@gmail.com>
2024-10-22 10:36:31 -0700

    test

--- components/mfrc522_i2c/mfrc522_i2c.h
+++ components/mfrc522_i2c/mfrc522_i2c.h
@@ -9,8 +9,8 @@ namespace mfrc522_i2c {
 
 class MFRC522I2C : public rc522::RC522, public i2c::I2CDevice {
  public:
-  void setup() override;   // Ensure setup() is defined
-  void loop() override;    // Ensure loop() is defined
+  void setup() override;
+  void loop() override;
   void dump_config() override;
 
   bool read_full_uid(uint8_t *uid, uint8_t *uid_length);
@@ -18,11 +18,14 @@ class MFRC522I2C : public rc522::RC522, public i2c::I2CDevice {
   bool read_atqa(uint16_t *atqa);
 
  protected:
-  // Ensure these virtual functions are correctly declared and implemented
   uint8_t pcd_read_register(rc522::RC522::PcdRegister reg) override;
   void pcd_read_register(rc522::RC522::PcdRegister reg, uint8_t count, uint8_t *values, uint8_t rx_align) override;
   void pcd_write_register(rc522::RC522::PcdRegister reg, uint8_t value) override;
   void pcd_write_register(rc522::RC522::PcdRegister reg, uint8_t count, uint8_t *values) override;
+
+  // New methods for data validation
+  bool validate_data(uint8_t *data, uint8_t length);
+  bool is_repeated_data(uint8_t *data, uint8_t length);
 };
 
 }  // namespace mfrc522_i2c
